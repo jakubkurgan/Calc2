d683e31c1d885d6119e04a46905960267ff530b8
Jakub Kurgan <jakubkurgan@gmail.com>
2019-04-06 09:17:44 +0100

    Create Xcode project

---
+++ Calc2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calc2
+//
+//  Created by user914614 on 4/6/19.
+//  Copyright © 2019 Jakub Kurgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
